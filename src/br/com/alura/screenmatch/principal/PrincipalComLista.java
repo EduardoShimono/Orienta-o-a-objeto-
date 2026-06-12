f7b2ebd02b79b0fa3bb35d8d823b1263e907da30
EduardoShimono <didushimono@gmail.com>
2026-06-12 10:39:25 -0300

    aprendendo a trabalhar com instanceof e referenciando o filmeDoEduardo atavez do f1.

--- src/br/com/alura/screenmatch/principal/PrincipalComLista.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComLista.java
@@ -9,9 +9,14 @@ import java.util.ArrayList;
 public class PrincipalComLista {
     public static void main(String[] args) {
         Filme favorito = new Filme("The Matrix", 1999);
+        favorito.avaliar(9);
         Filme outro = new Filme("John Wick", 2014);
+        outro.avaliar(8);
         var filmeDoEduardo = new Filme("Fome De Poder", 2024);
+        filmeDoEduardo.avaliar(10);
         Serie serie = new Serie("La Casa de Papel", 2017);
+
+        Filme f1 = filmeDoEduardo;
 
         ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoEduardo);
@@ -19,7 +24,10 @@ public class PrincipalComLista {
         lista.add(outro);
         lista.add(serie);
         for (Titulo item : lista) {
-            System.out.println(item);
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação " + ((Filme) item).getClassificacao());
+            }
         }
     }
 }
